b6e910175a7bb1d7ec65a76c217bb4d692a915ed
Arturo Valdelamar <pandacomputacion@gmail.com>
2025-06-20 20:44:04 +0000

    Revisa el codigo general por completo, busca y corrige errores y realiza

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Cache invalidation comment: 2024-07-25
   typescript: {
     ignoreBuildErrors: !!false, // Changed for production
   },
