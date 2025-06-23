76fc99fb992c315084a7a096c14123d04bf1d1bf
Arturo Valdelamar <pandacomputacion@gmail.com>
2025-06-23 14:13:13 +0000

    ⚠ Blocked cross-origin request from 3000-firebase-studio-1750318222114.c

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 👇  Aquí, NO dentro de "experimental"
   allowedDevOrigins: [
     'https://3000-firebase-studio-1750318222114.cluster-hf4yr35cmnbd4vhbxvfvc6cp5q.cloudworkstations.dev'
   ],
