941c8a430534eeba729bfd299ebd5a37ac022f8d
Arturo Valdelamar <pandacomputacion@gmail.com>
2025-06-25 20:00:09 +0000

    Cambia el texto Fecha y Hora del Servicio por FEcha y Hora del Servicio

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatCurrency = (amount: number | undefined) => {
+    if (typeof amount !== 'number' || isNaN(amount)) return '$0.00';
+    return `$${amount.toLocaleString('es-MX', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+};
