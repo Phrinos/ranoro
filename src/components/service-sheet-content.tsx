62fe000bae543d077e18a7418b4a089393a46a67
Arturo Valdelamar <pandacomputacion@gmail.com>
2025-07-10 05:02:38 +0000

    Make the following changes:
    On the page src/app/(app)/cotizaciones/hist

--- src/components/service-sheet-content.tsx
+++ src/components/service-sheet-content.tsx
@@ -6,7 +6,7 @@ import type { ServiceRecord, Vehicle, QuoteRecord, WorkshopInfo, SafetyInspectio
 import { format, parseISO, isValid } from 'date-fns';
 import { es } from 'date-fns/locale';
 import React from 'react';
-import { cn } from "@/lib/utils";
+import { cn, normalizeDataUrl } from "@/lib/utils";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
@@ -189,7 +189,7 @@ const SafetyChecklistDisplay = ({
             {inspection.technicianSignature && (
                  <div className="mt-8 border-t pt-4 text-center flex flex-col items-center">
                     <div className="h-24 w-full max-w-[200px] relative">
-                        <img src={inspection.technicianSignature} alt="Firma del técnico" style={{ objectFit: 'contain', width: '100%', height: '100%' }} crossOrigin="anonymous"/>
+                        <img src={normalizeDataUrl(inspection.technicianSignature)} alt="Firma del técnico" style={{ objectFit: 'contain', width: '100%', height: '100%' }} crossOrigin="anonymous"/>
                     </div>
                     <div className="border-t-2 border-black mt-2 pt-1 w-64 text-center">
                         <p className="text-xs font-bold">FIRMA DEL TÉCNICO</p>
@@ -389,7 +389,7 @@ export const ServiceSheetContent = React.forwardRef<HTMLDivElement, ServiceSheet
                   <h3 className="font-bold uppercase text-center text-sm">AUTORIZO QUE SE REALICEN ESTOS SERVICIOS</h3>
                   {service.customerSignatureReception ? (
                       <div className="w-full h-full flex items-center justify-center">
-                          <img src={service.customerSignatureReception} alt="Firma del cliente" style={{objectFit: 'contain', width: '200px', height: '100px'}} crossOrigin="anonymous" />
+                          <img src={normalizeDataUrl(service.customerSignatureReception)} alt="Firma del cliente" style={{objectFit: 'contain', width: '200px', height: '100px'}} crossOrigin="anonymous" />
                       </div>
                   ) : (
                     <div className="flex flex-col items-center justify-end flex-grow w-full">
@@ -415,7 +415,7 @@ export const ServiceSheetContent = React.forwardRef<HTMLDivElement, ServiceSheet
                     <div className="h-14 flex-grow flex items-center justify-center">
                         {service.serviceAdvisorSignatureDataUrl && (
                             <div className="relative w-full h-full max-w-[200px]">
-                                <img src={service.serviceAdvisorSignatureDataUrl} alt="Firma del asesor" style={{ objectFit: 'contain', width: '100%', height: '100%' }} crossOrigin="anonymous"/>
+                                <img src={normalizeDataUrl(service.serviceAdvisorSignatureDataUrl)} alt="Firma del asesor" style={{ objectFit: 'contain', width: '100%', height: '100%' }} crossOrigin="anonymous"/>
                             </div>
                         )}
                     </div>
@@ -427,7 +427,7 @@ export const ServiceSheetContent = React.forwardRef<HTMLDivElement, ServiceSheet
                    <div className="h-full flex-grow flex flex-col items-center justify-center">
                        {service.customerSignatureDelivery ? (
                          <div className="relative w-48 h-24">
-                           <img src={service.customerSignatureDelivery} alt="Firma de conformidad" style={{ objectFit: 'contain', width: '100%', height: '100%' }} crossOrigin="anonymous"/>
+                           <img src={normalizeDataUrl(service.customerSignatureDelivery)} alt="Firma de conformidad" style={{ objectFit: 'contain', width: '100%', height: '100%' }} crossOrigin="anonymous"/>
                          </div>
                        ) : (
                          isPublicView && showSignDelivery && onSignClick && (
