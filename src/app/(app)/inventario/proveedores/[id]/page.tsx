34b9075bc1f2805ffad3f49fc0870a081c60bd30
Arturo Valdelamar <pandacomputacion@gmail.com>
2025-08-03 06:33:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/inventario/proveedores/[id]/page.tsx
+++ src/app/(app)/inventario/proveedores/[id]/page.tsx
@@ -3,6 +3,6 @@
 import { redirect } from 'next/navigation';
 
 // This is a server component that handles redirection from the old URL.
-export default function OldSupplierDetailRedirect({ params }: { params: { id: string }}) {
+export default async function OldSupplierDetailRedirect({ params }: { params: { id: string }}) {
     redirect(`/proveedores/${params.id}`);
 }
