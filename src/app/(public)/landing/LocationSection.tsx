a48106f7df1049af353520600db6bd5498309a4e
Arturo Valdelamar <pandacomputacion@gmail.com>
2025-10-14 20:23:48 +0000

    Actualiza el pin del mapa ya que esta mostrando av de la convencion de 1

--- src/app/(public)/landing/LocationSection.tsx
+++ src/app/(public)/landing/LocationSection.tsx
@@ -29,7 +29,7 @@ function LocationSection() {
                     </AnimatedDiv>
                     <AnimatedDiv>
                         <div className="aspect-video rounded-lg overflow-hidden border shadow-lg">
-                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3701.353974488339!2d-102.31697288886915!3d21.92138137986429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8681fe76822c9a91%3A0x8313459c53644f15!2sAv.%20de%20la%20Convenci%C3%B3n%20de%201914%20Nte.%201421%2C%20Jardines%20de%20la%20Concepci%C3%B3n%20I%2C%2020120%20Aguascalientes%2C%20Ags.!5e0!3m2!1ses-419!2smx!4v1721774354228!5m2!1ses-419!2smx" width="100%" height="100%" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3702.570339299484!2d-102.3021980247293!3d21.8741344584067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8681fe76822c9a91%3A0x8313459c53644f15!2sAv.%20de%20la%20Convenci%C3%B3n%20de%201914%20Sur%201421%2C%20Jardines%20de%20la%20Concepci%C3%B3n%20I%2C%2020267%20Aguascalientes%2C%20Ags.!5e0!3m2!1ses-419!2smx!4v1721855619840!5m2!1ses-419!2smx" width="100%" height="100%" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </AnimatedDiv>
                 </div>
